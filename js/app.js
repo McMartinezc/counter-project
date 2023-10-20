557145ae972a2921dfbcb9152eae26f8c1d0f4d5
McMartinezc <psyence_mc@hotmail.com>
2023-10-20 12:22:44 +0200

    ok

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 // Tu código aquí.
+let counterValue = 0;
+
+const contador = document.getElementById("counter");
+const btnLower = document.querySelector(".prevBtn");
+const btnAdd = document.querySelector(".nextBtn");
+
+//Cambio de color del contador
+function updateColorCounter() {
+  contador.textContent = counterValue;
+  if (counterValue === 0) {
+    contador.style.color = "var(--mainBlack)";
+  } else {
+    contador.style.color = counterValue < 0 ? "red" : "green";
+  }
+}
+
+//Botón decrementar
+btnLower.addEventListener("click", () => {
+  counterValue--;
+  updateColorCounter();
+});
+
+//Botón incrementar
+btnAdd.addEventListener("click", () => {
+  counterValue++;
+  updateColorCounter();
+});
